f53ef04d840bb3989346cb35cf3ad8bb852090bf
Karen Souza <karensouza8@hotmail.com>
2024-06-22 16:46:39 -0300

    K: update help

--- src/components/common/Feed/FeedHelp.tsx
+++ src/components/common/Feed/FeedHelp.tsx
@@ -3,21 +3,21 @@ import Image from "next/image";
 export default function FeedHelp() {
   return (
     <>
-        <section className="pt-10 pb-10 bg-brand-100" id="services">
+        <section className="py-16 bg-brand-100" id="services">
             <div className="container">
                 <div className="row">
                     <div className="text-center mb-5">
                         <div className="flex items-center justify-center mb-5">
                             <div className="border-2 border-[#ffffff] w-10"></div>
                             <p className="text-white mx-2">
-                                IF YOU NEED CONTACT
+                                Se precisar entre em contato
                             </p>
                         </div>
                         <h1 className="text-white text-3xl mb-5 font-bold max-w-lg mx-auto">
-                            Do You Need Help With Electrical Maintenance?
+                            Você precisa de ajuda com a elétrica da sua casa?
                         </h1>
-                        <p className="text-white max-w-xl mx-auto">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Nulla porttitor accumsan tincidunt. Nulla quis lorem ut libero malesuada feugiat.
+                        <p className="text-white max-w-3xl mx-auto">
+                            Estou aqui para oferecer assistência em qualquer necessidade elétrica que você tenha em sua residência. Com qualificação e experiência, estou à disposição para resolver problemas, realizar manutenções e garantir a segurança elétrica do seu lar.
                         </p>
                     </div>
 
